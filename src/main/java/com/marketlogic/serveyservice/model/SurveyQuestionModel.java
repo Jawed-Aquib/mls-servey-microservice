cd4f40ccfe46a593f84c5407e7cc5fa9e48028a5
AquibJawed <jawedaquib2010@gmail.com>
2022-11-02 12:30:53 +0530

    Junits added for one case

--- src/main/java/com/marketlogic/serveyservice/model/SurveyQuestionModel.java
+++ src/main/java/com/marketlogic/serveyservice/model/SurveyQuestionModel.java
@@ -1,17 +1,41 @@
 package com.marketlogic.serveyservice.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.Objects;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class SurveyQuestionModel {
     private Integer id;
     private String question;
-    private List<SurveyAnswerModel> surveyAnswerBeans;
+    private List<SurveyAnswerModel> surveyAnswerModels;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SurveyQuestionModel that = (SurveyQuestionModel) o;
+        boolean surveyAnswerModelsCheck = true;
+        for(int i = 0; i < this.surveyAnswerModels.size();i++){
+            SurveyAnswerModel thisModel = this.surveyAnswerModels.get(0);
+            SurveyAnswerModel thatModel = that.surveyAnswerModels.get(0);
+            surveyAnswerModelsCheck = surveyAnswerModelsCheck
+                    && Objects.equals(thisModel.getId(), thatModel.getId())
+                    && Objects.equals(thisModel.getIsSelected(), thatModel.getIsSelected())
+                    && Objects.equals(thisModel.getValue(), thatModel.getValue());
+        }
+        return Objects.equals(id, that.id) && Objects.equals(question, that.question) && surveyAnswerModelsCheck;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, question, surveyAnswerModels);
+    }
 }
